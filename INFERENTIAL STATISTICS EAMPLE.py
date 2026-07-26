64cb726a823256688cf48231fd1549beaa24f1b6
SHREYA SHARMA <muskmelonn.10@gmail.com>
2026-07-26 14:25:22 +0530

    Create INFERENTIAL STATISTICS EAMPLE.py

---
+++ INFERENTIAL STATISTICS EAMPLE.py
@@ -0,0 +1,11 @@
+Example of Inferential Statistics
+
+Suppose there are 200 students in a college.
+
+Instead of measuring the height of every student, we collect the heights of 20 students.
+
+Question
+
+Are the heights of these 20 students representative of the entire college?
+
+Using Inferential Statistics, we can draw conclusions about the whole population from the selected sample.
