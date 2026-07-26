f76b6e78c4c457412665aca6062d693c69208da4
SHREYA SHARMA <muskmelonn.10@gmail.com>
2026-07-26 14:24:25 +0530

    Create INFERENTIAL STATISTICS.py

---
+++ INFERENTIAL STATISTICS.py
@@ -0,0 +1,15 @@
+2. Inferential Statistics
+
+Inferential Statistics is used to draw conclusions or make predictions about an entire population based on a sample of data.
+
+Instead of studying every individual, we analyze a sample and infer information about the population.
+
+Main Topics
+Hypothesis Testing
+P-value
+Confidence Interval
+Statistical Tests
+Z-Test
+T-Test
+ANOVA (F-Test)
+Chi-Square Test
