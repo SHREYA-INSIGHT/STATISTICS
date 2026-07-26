ecf6387019fee566062019ecf89799c4dc86851f
SHREYA SHARMA <muskmelonn.10@gmail.com>
2026-07-26 14:23:34 +0530

    Add example of descriptive statistics calculation
    This script provides an example of calculating the average height of students using descriptive statistics.

---
+++ DESCRIPTIVE STATISTICS EXAMPLE.py
@@ -0,0 +1,23 @@
+Example of Descriptive Statistics
+
+Suppose the heights of six students are:
+
+175 cm
+180 cm
+140 cm
+135 cm
+160 cm
+120 cm
+Question
+
+What is the average height of the students?
+
+Solution
+
+Mean = (175 + 180 + 140 + 135 + 160 + 120) / 6
+
+= 910 / 6
+
+≈ 151.67 cm
+
+This is an example of Descriptive Statistics because we are only describing the available data.
