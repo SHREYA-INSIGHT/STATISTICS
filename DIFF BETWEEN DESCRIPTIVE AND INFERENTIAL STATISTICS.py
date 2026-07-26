6998e233b2c208af1df2c4fd15abd9cb7f455943
SHREYA SHARMA <muskmelonn.10@gmail.com>
2026-07-26 14:29:35 +0530

    Create DIFF BETWEEN DESCRIPTIVE AND INFERENTIAL STATISTICS.py

---
+++ DIFF BETWEEN DESCRIPTIVE AND INFERENTIAL STATISTICS.py
@@ -0,0 +1,13 @@
+# Difference Between Descriptive and Inferential Statistics
+
+### Descriptive Statistics
+- Describes existing data.
+- Uses the complete available dataset.
+- Organizes and summarizes data.
+- Examples: **Mean, Median, Mode, Variance, and Standard Deviation.**
+
+### Inferential Statistics
+- Makes predictions about a population.
+- Uses sample data.
+- Draws conclusions and supports decision-making.
+- Examples: **Hypothesis Testing, Confidence Interval, Z-Test, and T-Test.**
