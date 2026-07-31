14579c383e08ecaa3d0177d838a637e1d9e22cd8
SHREYA SHARMA <muskmelonn.10@gmail.com>
2026-07-31 12:49:09 +0530

    Update POPULATION AND SAMPLE DATA.py

--- POPULATION AND SAMPLE DATA.py
+++ POPULATION AND SAMPLE DATA.py
@@ -1,1 +1,58 @@
+# Population
 
+A population is the complete set of individuals, objects, or observations that are of interest in a study. It includes every member of the group from which information is collected.
+example of population -1. Population in a School Study
+Population: All students enrolled in a school.
+Purpose: To determine the average marks of all students.
+2. Population in Market Research
+Population: All consumers living in a city.
+Purpose: To understand consumer purchasing behaviour.
+3. Population in a Medical Study
+Population: All patients with a specific disease.
+Purpose: To study the effectiveness of a drug.
+4. Population in an Election Survey
+Population: All eligible voters in a state.
+Purpose: To predict election results.
+5. Population in an Employee Survey
+Population: All employees working in a company.
+Purpose: To measure employee satisfaction.
+. Population in Agricultural Research
+Population: All farms in a district.
+Purpose: To estimate crop production.
+
+Simple definition to remember:
+
+Population = Entire group you want to study.
+
+For example:
+
+All students in a school → Population
+All customers of a bank → Population
+All patients in a hospital → Population
+  
+# Sample
+
+A sample is a subset of the population selected to represent the entire population. Sampling involves choosing a group of individuals or observations from the population to draw conclusions about the whole population.
+example of sample-1. Sample in a School Study
+Sample: 50 students selected from a school.
+Purpose: To estimate the average marks of all students.
+2. Sample in Market Research
+Sample: 500 consumers selected from a city.
+Purpose: To understand the purchasing behaviour of all consumers.
+3. Sample in a Medical Study
+Sample: 200 patients with a specific disease.
+Purpose: To test the effectiveness of a new drug.
+4. Sample in an Election Survey
+Sample: 1,000 voters selected from a state.
+Purpose: To predict the election results.
+5. Sample in an Employee Survey
+Sample: 100 employees selected from a company.
+Purpose: To measure employee satisfaction.
+6. Sample in Agricultural Research
+Sample: 50 farms selected from a district.
+Purpose: To estimate crop production.
+Easy Trick to Remember
+Population = Entire Group
+Example: All students in a school.
+Sample = Small Part of the Population
+Example: 50 students selected from that school.
